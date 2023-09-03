e77f1e380ad8fb9ea4c1af2a601d1c6249424170
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-03 22:25:33 +0530

    Undefined null never unknown

--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,32 @@ const someObject = {
   age: 5,
 };
 
-console.log(a, b, c, someArray, someObject);
+const abc = undefined;
+const def = null;
+
+function getData(): string | undefined {
+  return '';
+}
+
+const data = getData();
+
+if (data) {
+  const someOtherData = data;
+  console.log(someOtherData);
+}
+
+let input: unknown;
+input = 'hello';
+
+let somePrivateValue: string;
+if (typeof input === 'string') {
+  somePrivateValue = input;
+}
+
+function doTasks(tasks: number): void | never {
+  if (tasks > 3) {
+    throw new Error('Too many tasks');
+  }
+}
+
+const stuff = doTasks(2);
