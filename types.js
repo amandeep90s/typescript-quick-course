7c4f400b250ae5b438716232f39944b210a965b1
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-03 13:53:52 +0530

    Types

---
+++ types.js
@@ -0,0 +1,9 @@
+var a = 'Hello world';
+var b = 27;
+var c = true;
+var someArray = ['a', 'b', 'c'];
+var someObject = {
+    name: 'Aman',
+    age: 5,
+};
+console.log(a, b, c, someArray, someObject);
---
+++ types.ts
@@ -0,0 +1,10 @@
+const a: string = 'Hello world';
+const b: number = 27;
+const c: boolean = true;
+const someArray: Array<string> = ['a', 'b', 'c'];
+const someObject = {
+  name: 'Aman',
+  age: 5,
+};
+
+console.log(a, b, c, someArray, someObject);
