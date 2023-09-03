f6152b91f64229ec086145d58010afa4f21f77a1
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-03 16:14:08 +0530

    Updated gitignore file

--- types.js
+++
@@ -1,9 +0,0 @@
-var a = 'Hello world';
-var b = 27;
-var c = true;
-var someArray = ['a', 'b', 'c'];
-var someObject = {
-    name: 'Aman',
-    age: 5,
-};
-console.log(a, b, c, someArray, someObject);
--- user-defined-types.js
+++
@@ -1,9 +0,0 @@
-function generateEmail(input) {
-    return "".concat(input.firstName, ".").concat(input.lastName, "@gmail.com");
-}
-var someUser = {
-    firstName: 'Amandeep',
-    lastName: 'Singh',
-    job: 'Programmer',
-};
-console.log(generateEmail(someUser));
