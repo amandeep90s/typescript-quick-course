8c7ea19b77f69573927140caf4dd99a82c6a2af5
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-04 22:25:26 +0530

    Run in browser

---
+++ ts-frontend-practice/src/Launcher.ts
@@ -0,0 +1,10 @@
+class Launcher {
+  private router: Router = new Router();
+
+  public launchApp() {
+    console.log('App started');
+    this.router.handleRequest();
+  }
+}
+
+new Launcher().launchApp();
---
+++ ts-frontend-practice/src/Router.ts
@@ -0,0 +1,9 @@
+class Router {
+  public handleRequest() {
+    const location = this.getRoute();
+  }
+
+  private getRoute() {
+    return window.location.pathname;
+  }
+}
