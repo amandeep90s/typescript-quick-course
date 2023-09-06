d6560bbd54447c72b787a9f3be72fb73a609ee20
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-06 21:36:15 +0530

    Method decorators

--- src/classes.ts
+++ src/classes.ts
@@ -1,27 +1,43 @@
-import { Comp1 } from '@components/Comp1';
+function logInvocation(
+  target: Object,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const className = target.constructor.name;
+  let originalMethod = descriptor.value;
+  descriptor.value = async function (...args: any[]) {
+    console.log(
+      `${className}#${propertyKey} called with ${JSON.stringify(args)}`
+    );
+    const result = await originalMethod.apply(this, args);
+    console.log(
+      `${className}#${propertyKey} returned ${JSON.stringify(result)}`
+    );
+  };
+}
 
 class Server {
   private port: number;
   private host: string;
-  private comp: Comp1;
 
-  constructor(port, host) {
+  constructor(port: number, host: string) {
     this.port = port;
     this.host = host;
-    this.comp = new Comp1();
   }
 
   startServer() {
     console.log(`Server is running at ${this.host}:${this.port}`);
   }
   async fetchData() {
-    return await this.getData();
+    return await this.getData(123);
   }
 
-  async getData() {
-    return '{}';
+  @logInvocation
+  async getData(id: number): Promise<string> {
+    return 'some special data';
   }
 }
 
 const newServer = new Server(5000, 'localhost');
 newServer.startServer();
+newServer.getData(456);
